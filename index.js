a10e5138cf8a74fe07adff5bff4c9c90911e2c18
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-05-23 02:44:54 +0300

    first type numbers errors

---
+++ index.js
@@ -0,0 +1,7 @@
+function add(firstValue, secondValue) {
+    return firstValue + secondValue;
+}
+var firstNumber = '5';
+var secondNumber = 2.8;
+var result = add(firstNumber, secondNumber);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,9 @@
+function add(firstValue, secondValue) {
+    return firstValue + secondValue;
+}
+
+const firstNumber = '5';
+const secondNumber = 2.8;
+
+const result = add(firstNumber, secondNumber);
+console.log(result);
