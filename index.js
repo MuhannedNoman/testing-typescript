153e9d67d0d958e676bc9b980b8eb9940ae272da
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-05-23 16:11:13 +0300

    Added enum type

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hoobies: string[];
+//     role: [number, string];
+//     // Setting a specific types and length for this array {Typle}
+// } = {
+//     name: 'James',
+//     age: 28,
+//     hoobies: ['Sports','Coding'],
+//     role: [2, 'author']
+// };
+// enum Role { ADMIN, READ_ONLY, AUTHOR};
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role["READ_ONLY"] = "Read";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
+;
+// Setting different value for the enum.
 var person = {
     name: 'James',
     age: 28,
-    hoobies: ['Sports', 'Coding']
+    Role: Role.ADMIN
 };
+if (person.Role === Role.ADMIN) {
+    console.log('You are an Admin !!');
+}
 var definationOfArray;
 definationOfArray = ['Hello', 'World'];
-for (var _i = 0, _a = person.hoobies; _i < _a.length; _i++) {
-    var hoopy = _a[_i];
-    console.log(hoopy);
-}
+// for (const hoopy of person.hoobies){
+//     console.log(hoopy)
+// }
 // const person: {
 //     name: string;
 //     age: number;
--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
-const person: {
-    name: string;
-    age: number;
-    hoobies: string[];
-    role: [number, string];
-    // Setting a specific types and length for this array {Typle}
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hoobies: string[];
+//     role: [number, string];
+//     // Setting a specific types and length for this array {Typle}
+// } = {
+//     name: 'James',
+//     age: 28,
+//     hoobies: ['Sports','Coding'],
+//     role: [2, 'author']
+// };
+
+// enum Role { ADMIN, READ_ONLY, AUTHOR};
+
+enum Role { ADMIN = 5, READ_ONLY = 'Read', AUTHOR = 200};
+// Setting different value for the enum.
+
+const person = {
     name: 'James',
     age: 28,
-    hoobies: ['Sports','Coding'],
-    role: [2, 'author']
+    Role: Role.ADMIN
 };
+
+if (person.Role === Role.ADMIN) {
+    console.log('You are an Admin !!')
+}
+
 
 let definationOfArray: string[];
 definationOfArray = ['Hello','World'];
 
-for (const hoopy of person.hoobies){
-    console.log(hoopy)
-}
+// for (const hoopy of person.hoobies){
+//     console.log(hoopy)
+// }
 
 // const person: {
 //     name: string;
